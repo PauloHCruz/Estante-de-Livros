ff039be8077efd51994bce9af598ef10af3bafaa
PauloHCruz <pauloh.g.j.j@gmail.com>
2025-03-05 18:28:24 -0300

    adicionando funcionaldiade de add/del de livros na estante e seu status de leitura

--- app.py
+++ app.py
@@ -1,24 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "chave_super_secreta"
 
-@app.route('/')
-def index():
-    livros = [
-        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
-        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
-        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
-        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
-        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"},
-        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"},
-        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"},
-        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"}
-    ]
-    return render_template('livros.html',livros=livros)
+livros = [
+    {"id": "1", "titulo": "Flask Web Development", "autor": "Miguel Grinberg", "imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg"},
+    {"id": "2", "titulo": "Flask Web Development", "autor": "Miguel Grinberg", "imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg"},
+    {"id": "3", "titulo": "Flask Web Development", "autor": "Miguel Grinberg", "imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg"}
+]
 
-@app.route('/minha-estante')
+@app.route("/")
+def home():
+    minha_estante = session.get("minha_estante", {})
+    return render_template("livros.html", livros=livros, minha_estante=minha_estante)
+
+@app.route("/minha-estante")
 def minha_estante():
-    return render_template('minha-estante.html')
+    minha_estante = session.get("minha_estante", {})
+    livros_na_estante = [livro for livro in livros if livro["id"] in minha_estante]
+    return render_template("minha-estante.html", livros=livros_na_estante, minha_estante=minha_estante)
 
-if __name__ == '__main__':
+@app.route("/alterna_estante", methods=["POST"])
+def alterna_estante():
+    book_id = request.form.get("book_id")
+    minha_estante = session.get("minha_estante", {})
+
+    if book_id in minha_estante:
+        del minha_estante[book_id]
+    else:
+        minha_estante[book_id] = "quero ler"
+
+    session["minha_estante"] = minha_estante
+    session.modified = True
+
+    return redirect(request.referrer or url_for("home"))
+
+@app.route("/atualiza_status", methods=["POST"])
+def atualiza_status():
+    book_id = request.form.get("book_id")
+    status = request.form.get("status")
+    minha_estante = session.get("minha_estante", {})
+
+    if book_id in minha_estante:
+        minha_estante[book_id] = status
+
+    session["minha_estante"] = minha_estante
+    session.modified = True
+
+    return redirect(request.referrer or url_for("minha_estante"))
+
+if __name__ == "__main__":
     app.run(debug=True)
