8470fbb883a5e5535538d5201106169d4533ee33
PauloHCruz <pauloh.g.j.j@gmail.com>
2025-03-05 16:21:45 -0300

    macro para card de livros

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('livros.html')
+    livros = [
+        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
+        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
+        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
+        {"imagem":"https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo":"Titulo", "autor": "Autor"},
+        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"},
+        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"},
+        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"},
+        {"imagem": "https://m.media-amazon.com/images/I/91jCDEyM3fL.jpg", "titulo": "Titulo", "autor": "Autor"}
+    ]
+    return render_template('livros.html',livros=livros)
 
 @app.route('/minha-estante')
 def minha_estante():
