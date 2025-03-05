3ed7be35fa8350021c71f4252402ad3295555c14
PauloHCruz <pauloh.g.j.j@gmail.com>
2025-03-05 13:24:05 -0300

    paginaçao

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('livros.html')
+
+@app.route('/minha-estante')
+def minha_estante():
+    return render_template('minha-estante.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
